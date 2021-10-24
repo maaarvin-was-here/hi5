9d06a7f89bf43fc2fc3901cdc44f3b8ef95aa7f1
Marvin <Marvin@LAPTOP-SMH9VBCL>
2021-10-24 03:48:20 -0700

    pull attempt

---
+++ components/CurrentMessageBox.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { Image, StyleSheet } from 'react-native';
+
+import { Text, View } from '../components/Themed';
+import { renderSwitch } from './NearbyPersonBox';
+
+type CurrentMessageBoxProps = {
+  message: string;
+  name: string;
+  avatar: number;
+};
+
+export default function CurrentMessageBox(props: CurrentMessageBoxProps) {
+  const { message, name, avatar } = props;
+  var pic = renderSwitch(avatar);
+  return (
+    <View style={styles.outer_box}>
+      <Text>
+        <View style={styles.container}>
+          <Image source={pic} style={{ width: 80, height: 80 }} />
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.name}>
+            {name}
+            {'\n'}
+          </Text>
+          <Text style={styles.italic}>{message}</Text>
+        </View>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+    backgroundColor: '#72dcc3',
+  },
+  name: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  outer_box: {
+    flex: 1,
+    padding: 8,
+    backgroundColor: '#72dcc3',
+    borderRadius: 10,
+    margin: 5,
+  },
+  italic: { fontStyle: 'italic', marginTop: -20 },
+});
